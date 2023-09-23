0f77b980c2bb7b4c50e32592b25f0e0c85066815
Mohit Singhal <webdeveloper1830@gmail.com>
2023-09-23 15:29:32 +0530

    update route file

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
